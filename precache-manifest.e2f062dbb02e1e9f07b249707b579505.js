50b0ea6a2b80ff8625abcc7534a0c58415782df9
MarkMGill <40337791+MarkMGill@users.noreply.github.com>
2019-09-21 10:00:07 -0400

    Updates

---
+++ precache-manifest.e2f062dbb02e1e9f07b249707b579505.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0962dc020962e51cc212b32b4917e82f",
+    "url": "/PicStore/index.html"
+  },
+  {
+    "revision": "f2c35ca6da4297e3994e",
+    "url": "/PicStore/static/css/main.3f0b79b0.chunk.css"
+  },
+  {
+    "revision": "afe71592dfab42ca07de",
+    "url": "/PicStore/static/js/2.533bb83b.chunk.js"
+  },
+  {
+    "revision": "f2c35ca6da4297e3994e",
+    "url": "/PicStore/static/js/main.5d7746fe.chunk.js"
+  },
+  {
+    "revision": "dcc178e40d140c95afdb",
+    "url": "/PicStore/static/js/runtime~main.7a5cfbc0.js"
+  }
+]);
